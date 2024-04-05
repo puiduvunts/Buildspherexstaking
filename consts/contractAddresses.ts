ff31a7a6a42b1d7f1061a47020cc30ad44a8bc46
puiduvunts <161220585+puiduvunts@users.noreply.github.com>
2024-04-05 14:56:47 +0300

    Add files via upload

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x8Ae711a3A64c96fF55dAFa1f4dfef85ADC254e0f";
+export const tokenContractAddress =
+  "0x2Bd5742ED098fCa65552E4034d8E9B6E97AE5141";
+export const stakingContractAddress =
+  "0x154cd2Dd17D75F1Af0bB38438dAb1cA6a3B98f80";
